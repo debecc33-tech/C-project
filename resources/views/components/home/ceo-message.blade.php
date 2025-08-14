61fba6d2dc4b3f7cd3cdcddf29987dd5884d558a
SlowWebDev <yf34562@gmail.com>
2025-08-14 14:30:59 +0300

    just udpdate some data forget to add it

---
+++ resources/views/components/home/ceo-message.blade.php
@@ -0,0 +1,24 @@
+<section class="bg-white my-16">
+  <div class="container mx-auto">
+    <div class="grid md:grid-cols-2 gap-0 shadow-lg rounded-[50px] overflow-hidden bg-[#F1F1F1] md:min-h-[600px]">
+      
+      <!-- Text Side -->
+      <div class="p-12 flex flex-col justify-center" data-aos="fade-right">
+        <h2 class="text-[40px] font-bold mb-6">CEO Message</h2>
+        <p class="text-gray-600 leading-relaxed">
+          At TG Developments, our journey has been shaped by over 20 years of dedicated service in the real estate industry. Throughout this time, we have grown and evolved, always with a focus on delivering high-quality, innovative projects that enhance the communities we touch.
+        </p>
+      </div>
+
+      <!-- Image Side -->
+      <div class="min-h-[600px] h-full bg-[#F1F1F1] relative" data-aos="fade-left">
+        <img 
+          src="{{asset('assets/images/ceo-message/ceo-img.jpeg')}}" 
+          class="absolute inset-0 h-full w-full object-contain"
+          alt="CEO - IMG"
+        >
+      </div>
+
+    </div>
+  </div>
+</section>
---
+++ resources/views/components/home/why-choose-us.blade.php
@@ -0,0 +1,27 @@
+<section class="why-choose-section" style="background-image: url('/assets/images/why-choose-us/why-choose-us-background.png')">
+    <div class="why-choose-container">
+        <!-- Image section -->
+        <div class="why-choose-image" data-aos="fade-right" data-aos-duration="1000" data-aos-offset="200">
+            <img 
+                src="/assets/images/why-choose-us/why-choose-us-text.png" 
+                alt="Why Choose Us"
+            >
+        </div>
+        
+        <!-- Stats section -->
+        <div class="why-choose-stats">
+            <div class="stat-circle" data-aos="fade-up" data-aos-delay="100">
+                <span class="stat-circle-number stat-counter" data-target="20" data-prefix="+">0</span>
+                <span class="stat-circle-label">Year</span>
+            </div>
+            <div class="stat-circle" data-aos="fade-up" data-aos-delay="200">
+                <span class="stat-circle-number stat-counter" data-target="60" data-prefix="+">0</span>
+                <span class="stat-circle-label">Project</span>
+            </div>
+            <div class="stat-circle" data-aos="fade-up" data-aos-delay="300">
+                <span class="stat-circle-number stat-counter" data-target="1200" data-prefix="+">0</span>
+                <span class="stat-circle-label">Unit</span>
+            </div>
+        </div>
+    </div>
+</section>
